7e7775b0e8018906213da3bc76bb2a366964dcb6
Edamijueda <tobbyzomo221@gmail.com>
2023-11-22 09:25:38 +0100

    change app name

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Tobi Omorubore',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Hello! My name is Tobi and I enjoy creating things that live on the internet. My interest is in mobile app and web development!',
+    siteUrl: 'https://tobi-omorubore.dev/', // No trailing slash allowed!
     image: '/logo.png', //'/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@OmoruboreO',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'Tobi Omorubore',
+        short_name: 'Tobi Omorubore',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
